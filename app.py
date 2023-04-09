ecae9e5caca75edd6b533348a38a97f31063998a
alikon <optimus4joomla@gmail.com>
2023-04-09 10:34:16 +0200

    d

--- app.py
+++ app.py
@@ -73,6 +73,7 @@ if account:
                 Top {numero} authors for [@{account}](https://twitter.com/{account}) from {da} to {to}
             """
         )
+        st.write(len(top_authors))
         for i, author in enumerate(top_authors):
             cols = st.columns([1, 2, 13])
             cols[0].markdown(f"**{i + 1}**")
