cd5ad1a7f7f5d6264ad439ce8648d1f4bedb0a0e
alikon <optimus4joomla@gmail.com>
2023-04-09 11:06:56 +0200

    today

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ datefrom = st.sidebar.date_input(
 
 dateto = st.sidebar.date_input(
     "To ",
-    datetime.now())
+    datetime.today().strftime('%Y-%m-%d'))
 
 numero = st.sidebar.slider('Top Liked', 1, 100, 10)
 
