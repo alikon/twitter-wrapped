24a83e1b0e492c9970bf7518b84c1e2c13776082
alikon <optimus4joomla@gmail.com>
2023-04-10 09:26:40 +0200

    ww

--- app.py
+++ app.py
@@ -4,36 +4,8 @@ import datetime
 # Import 3rd party libraries
 import streamlit as st
 import streamlit.components.v1 as components
-import tweepy
 # Import modules
 import twitter as twi
-
-@st.cache_data(ttl=60*60*12, show_spinner=True)
-def search():
-    # OAuth2.0 Version 
-
-    #Put your Bearer Token in the parenthesis below
-    client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAMHokQAAAAAAIfGSDpNh0ElBxqEJaXw2%2FblXBR8%3DmGJTbzqSNctxO2WYDehtLdpNOKvDYEqyjdUBZvaR1ogOTgsq2l')
-
-    """
-    If you don't understand search queries, there is an excellent introduction to it here: 
-    https://github.com/twitterdev/getting-started-with-the-twitter-api-v2-for-academic-research/blob/main/modules/5-how-to-write-search-queries.md
-    """
-
-    # Get tweets that contain the hashtag #petday
-    # -is:retweet means I don't want retweets
-    # lang:en is asking for the tweets to be in english
-    query = '#joomla -is:retweet lang:en'
-    tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100)
-
-    """
-    What context_annotations are: 
-    https://developer.twitter.com/en/docs/twitter-api/annotations/overview
-    """
-    for tweet in tweets.data:
-        print(tweet.text)
-        if len(tweet.context_annotations) > 0:
-            print(tweet.context_annotations)
 
 # Define functions
 @st.cache_data(ttl=60*60*12, show_spinner=True)
